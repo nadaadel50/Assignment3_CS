ccbc3d995bf95384c37a82daaab15296fd35b6e7
ToukaAtef <125757900+ToukaAtef@users.noreply.github.com>
2023-11-11 21:39:54 +0200

    Update CPU.h

--- CPU.h
+++ CPU.h
@@ -1,4 +1,4 @@
-class CPU {
+/*class CPU {
 protected:
     int counter = 0;
 
@@ -9,6 +9,31 @@ public:
     int Accumulator;
 
 };
+*/
+#include <iostream>
+#include<bits/stdc++.h>
+#include <string>
+#include <vector>
+#include <utility> // for pair
+//#include "memory.h"
+using namespace std;
+
+
+class cpu {
+protected:
+    vector<pair<int , int >>reg;
+    vector<pair<string, int >>storage;
+public:
+    void input_reg(vector<pair<string,pair<string,string>>>& memo);
+   // void input_storage(vector<pair<string,pair<string,string>>>& memo);
+    void printReg(const   vector<pair<int , int >>& reg);
+    const vector<pair<int,int>>&getReg()const;
+
+};
+
+
+
+
 
 
 
