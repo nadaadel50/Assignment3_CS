1b81af7e54d75d0b45c2f38f227b65f44d9eb15b
ToukaAtef <125757900+ToukaAtef@users.noreply.github.com>
2023-11-11 21:38:48 +0200

    Update CPU.cpp

--- CPU.cpp
+++ CPU.cpp
@@ -1,11 +1,30 @@
-#include "CPU.h"
+//#include <utility> // for pair
+#include"cpu.h"
+//#include"memory.cpp"
+using namespace std;
 
-// Register // IR // PC
-// Fetch data from memory
-// Decode depending on sheet
-// Execute
+void cpu:: input_reg(vector<pair<string,pair<string,string>>>& memo) {
+    storage.push_back(make_pair("A2",7));
+    for (const auto& it : memo) {
+        if(it.first=="1"){
+            for(const auto& pair : storage){
+                if(pair.first==it.second.second){
+                    int s=pair.second;
+                    int ss=stoi(it.second.first);
+                    reg.push_back(make_pair(ss,s));
+                }
+            }
+
+        }
+    }
+}
 
 
-
-
-
+void cpu::printReg(const vector<pair<int,int>>& reg){
+    for (const auto& it : reg) {
+        cout << "First Part: " << it.first << " Second Part: " << it.second<< endl;
+    }
+}
+const vector<pair<int,int>>&cpu::getReg()const{
+    return reg;
+}
