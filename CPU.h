aab19c9e235f16d8770aaeab2126850878c893a3
Nada Adel <120988662+nadaadel50@users.noreply.github.com>
2023-11-15 18:58:40 +0200

    Update CPU.h

--- CPU.h
+++ CPU.h
@@ -7,6 +7,7 @@ protected:
     vector<string>v;
     vector<pair<string,pair<string,string>>>memo;
     vector<int>Jumpv;
+    vector<string>hexVector;
     int counter = 0;
 public:
     void inputMemory(vector<pair<string,pair<string,string>>>& memo);
@@ -18,11 +19,9 @@ class cpu {
 protected:
     vector<pair<string , string >>reg;
     vector<pair<string, string >>storage;
-    vector<int>Jumpv;
-    int counter = 0;
 
 public:
-    void input_reg(vector<pair<string,pair<string,string>>>& memo);
+    void input_reg(vector<pair<string,pair<string,string>>>& memo, vector<string>&hexVector);
 
     void printReg(const   vector<pair<string , string >>& reg);
     void printstr(const   vector<pair<string , string >>& storage);
@@ -31,6 +30,5 @@ public:
     const vector<pair<string ,string>>&getstr()const;
 
     bool size_of_storage(const   vector<pair<string , string >>& storage);
-
-
 };
+
